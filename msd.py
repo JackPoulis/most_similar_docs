320375ade3c6228d0195d7765a1e7104f5e13d04
Iakovos Poulis <jackpoulis@gmail.com>
2022-03-20 18:57:18 +0200

    Add code in main block to use script as a module

--- msd.py
+++ msd.py
@@ -18,7 +18,7 @@ def similarity(filename_a, filename_b):
     if files are identical
     :rtype: float
     """   
-     
+
     # Opening files using utf-8 encoding to recognize unicode characters
     file_a = codecs.open(filename_a, "r", encoding='utf-8')
     file_b = codecs.open(filename_b, "r", encoding='utf-8')
@@ -61,69 +61,69 @@ def similarity(filename_a, filename_b):
         similarity = inner_product/(norm_a*norm_b)
     return similarity
 
-
-N = 5  # Number of files
-K = 3  # Top K
-# The directory in which to parse for txt files (default is current dir)
-D = "."
-Nflag = False  # True if user provide N
-
-# Examination of user input
-for index in range(len(sys.argv)):
-    if sys.argv[index] == 'N' and len(sys.argv) > index+1 and sys.argv[index+1].isdigit():
-        N = int(sys.argv[index+1])
-        Nflag = True  # User provided N
-    elif sys.argv[index] == 'K' and len(sys.argv) > index+1 and sys.argv[index+1].isdigit():
-        K = int(sys.argv[index+1])
-    elif sys.argv[index] == 'D' and len(sys.argv) > index+1:
-        D = sys.argv[index+1]
-
-# Examination if user gave valid directory
-if not os.path.isdir(D):
-    print('"{}" is not a valid directory. Current directory "." will be used instead...'.format(D))
+if __name__ == "__main__":
+    N = 5  # Number of files
+    K = 3  # Top K
+    # The directory in which to parse for txt files (default is current dir)
     D = "."
+    Nflag = False  # True if user provide N
 
-# List of file names in directory D
-directory_files = glob.glob(os.path.join(D, "*.txt"))
-directory_files = [os.path.basename(item) for item in directory_files]
-directory_files = sorted(directory_files)
-if not directory_files:
-    print('No ".txt" files found in directory "{}"'.format(D))
+    # Examination of user input
+    for index in range(len(sys.argv)):
+        if sys.argv[index] == 'N' and len(sys.argv) > index+1 and sys.argv[index+1].isdigit():
+            N = int(sys.argv[index+1])
+            Nflag = True  # User provided N
+        elif sys.argv[index] == 'K' and len(sys.argv) > index+1 and sys.argv[index+1].isdigit():
+            K = int(sys.argv[index+1])
+        elif sys.argv[index] == 'D' and len(sys.argv) > index+1:
+            D = sys.argv[index+1]
 
-# Correction of N (if not provided or out of bounds) and K (if out of bounds)
-if len(directory_files) > 1:
-    if not Nflag or N > len(directory_files):
-        N = len(directory_files)
-    if K > ((N-1)*N)/2:
-        K = int(((N-1)*N)/2)
+    # Examination if user gave valid directory
+    if not os.path.isdir(D):
+        print('"{}" is not a valid directory. Current directory "." will be used instead...'.format(D))
+        D = "."
 
-    target_docs = []  # Files that will be parsed
-    for x in range(N):
-        target_docs.append(directory_files[x])
-    print("Files:", target_docs)
-    print("N =", N, "K =", K)
+    # List of file names in directory D
+    directory_files = glob.glob(os.path.join(D, "*.txt"))
+    directory_files = [os.path.basename(item) for item in directory_files]
+    directory_files = sorted(directory_files)
+    if not directory_files:
+        print('No ".txt" files found in directory "{}"'.format(D))
 
-    # Triangular 2D matrix with all cosines (initialized with zeros)
-    similarities_table = np.zeros((N, N), dtype=float)
+    # Correction of N (if not provided or out of bounds) and K (if out of bounds)
+    if len(directory_files) > 1:
+        if not Nflag or N > len(directory_files):
+            N = len(directory_files)
+        if K > ((N-1)*N)/2:
+            K = int(((N-1)*N)/2)
 
-    # Calculation of cos(A,B) for every pair of files A,B
-    for i in range(0, N-1):
-        filename_A = os.path.join(D, directory_files[i])
-        for j in range(N-1, i, -1):
-            filename_B = os.path.join(D, directory_files[j])
-            similarities_table[i][j] = similarity(filename_A, filename_B)
+        target_docs = []  # Files that will be parsed
+        for x in range(N):
+            target_docs.append(directory_files[x])
+        print("Files:", target_docs)
+        print("N =", N, "K =", K)
 
-    # Output for TOP-K
-    for top_k in range(K):
-        smax = -1
+        # Triangular 2D matrix with all cosines (initialized with zeros)
+        similarities_table = np.zeros((N, N), dtype=float)
+
+        # Calculation of cos(A,B) for every pair of files A,B
         for i in range(0, N-1):
+            filename_A = os.path.join(D, directory_files[i])
             for j in range(N-1, i, -1):
-                if similarities_table[i][j] > smax:
-                    smax = similarities_table[i][j]
-                    mi = i
-                    mj = j
-        if similarities_table[mi][mj] > -1:
-            print('{}) Similarity:{:.2f} "{}","{}"'.format(
-                top_k+1, similarities_table[mi][mj], directory_files[mi], directory_files[mj]))
-            # Setting max to an invalid small value ensures that it wont be printed again
-            similarities_table[mi][mj] = -1
+                filename_B = os.path.join(D, directory_files[j])
+                similarities_table[i][j] = similarity(filename_A, filename_B)
+
+        # Output for TOP-K
+        for top_k in range(K):
+            smax = -1
+            for i in range(0, N-1):
+                for j in range(N-1, i, -1):
+                    if similarities_table[i][j] > smax:
+                        smax = similarities_table[i][j]
+                        mi = i
+                        mj = j
+            if similarities_table[mi][mj] > -1:
+                print('{}) Similarity:{:.2f} "{}","{}"'.format(
+                    top_k+1, similarities_table[mi][mj], directory_files[mi], directory_files[mj]))
+                # Setting max to an invalid small value ensures that it wont be printed again
+                similarities_table[mi][mj] = -1
